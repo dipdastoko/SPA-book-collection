8af0f90938e05547fbeb41fcf0d9ec8766ed09fb
dipdastoko <dipdastoko@gmail.com>
2022-03-26 19:46:27 +0600

    finished

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,14 +4,24 @@ import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import './Cart.css';
 
 const Cart = props => {
+    let i = 0;
+    const { cart } = props;
     let total = 0;
-    for (const book of props.cart) {
+    for (const book of cart) {
         total = total + book.price;
     }
     return (
-        <div className='cart'>
-            <h3><FontAwesomeIcon icon={faBookOpen} /> Books Added: {props.cart.length}</h3>
-            <h3>Total Cost: {total}</h3>
+        <div>
+            <div className='cart-container'>
+                <h3><FontAwesomeIcon icon={faBookOpen} /> Books Added: {cart.length}</h3>
+                <h3>Total Cost: ${total}</h3>
+            </div>
+            <div className='addedBooksName'>
+                <h2>Added Books Name</h2>
+                {
+                    cart.map(book => <p key={book.name}><b>{++i}. {book.name}</b></p>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,8 @@ const Header = () => {
     return (
         <div className='header'>
             <h1>Books Collection</h1>
+            <p><i>Find Your Favourite Books Here</i></p>
+            <h3>Total Budget: $100</h3>
         </div>
 
     );
